5a28ec9d9e15b287194725e00da84b15a4e96074
Merthius <83304847+Merthius@users.noreply.github.com>
2023-02-09 04:33:37 +0100

    asf

---
+++ feed-button.js
@@ -0,0 +1,20 @@
+// Get the feed button
+const feedButton = document.querySelector(".feed-button");
+
+// Get the feed container
+const feedContainer = document.querySelector(".feed-container");
+
+// Hide the feed container by default
+feedContainer.style.display = "none";
+
+// Add a click event listener to the feed button
+feedButton.addEventListener("click", function() {
+  // If the feed container is hidden
+  if (feedContainer.style.display === "none") {
+    // Show the feed container
+    feedContainer.style.display = "block";
+  } else {
+    // Hide the feed container
+    feedContainer.style.display = "none";
+  }
+});
